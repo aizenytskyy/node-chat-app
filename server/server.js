66328272f4f680d03c3066e9fc4edcd3fea01fd0
Andriy Izenytskyy <aizenytskyy@gmail.com>
2018-11-25 13:26:20 +0200

    connect to server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const path = require('path');
-var express = require('express');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
-var app = express();
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use('/', express.static(publicPath));
-app.listen(port, () => {
+
+io.on('connection', socket => {
+  console.log('New user connected');
+});
+
+server.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
 
